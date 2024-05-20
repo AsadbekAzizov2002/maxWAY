6ba8b75511f245a993e8f586b6167f5761cb2fde
Your Name <you@example.com>
2024-05-20 16:41:05 +0300

    commit

--- src/Corusel.jsx
+++ src/Corusel.jsx
@@ -33,34 +33,44 @@ export default function Corusel() {
     <div className=" px-10">
       <Slider {...settings}>
         <div>
-          <div className=" px-10 mt-10">
+          <div className=" px-10 mt-5">
             <div className=" flex justify-center items-center">
               <img
                 className=" w-full rounded-3xl"
                 src="https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fceefc9aa-89e1-4464-9415-7cabc48a65d1&w=1200&q=75"
               />
             </div>
-          </div>{" "}
+          </div>
         </div>
         <div>
-          <div className=" px-10 mt-16">
+          <div className=" px-10 mt-5">
             <div className=" flex justify-center items-center">
               <img
                 className=" w-full rounded-3xl"
                 src="https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F2edd9920-1b42-4e3f-9491-a2359a08d334&w=1200&q=75"
               />
             </div>
-          </div>{" "}
+          </div>
         </div>
         <div>
-          <div className=" px-10 mt-16">
+          <div className=" px-10 mt-5">
+            <div className=" flex justify-center items-center">
+              <img
+                className=" w-full rounded-3xl"
+                src="https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fc548b8fc-3f0d-4180-ad2d-b0419c040101&w=1200&q=75"
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className=" px-10 mt-5">
             <div className=" flex justify-center items-center">
               <img
                 className=" w-full rounded-3xl"
                 src="https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Feedc6ad0-1c3c-40fb-a3f2-15b807b7ed30&w=1200&q=75"
               />
             </div>
-          </div>{" "}
+          </div>
         </div>
       </Slider>
     </div>
